d49bef1d31de6c61956d7dab5c877e06208b80b2
Sepehr Sattari <40902851+seqehr@users.noreply.github.com>
2022-12-29 15:56:39 +0330

    change cat list url

--- src/services/ArticleServices.js
+++ src/services/ArticleServices.js
@@ -1,20 +1,20 @@
-import config from './config.json'
-import httpService from './httpService'
+import config from "./config.json";
+import httpService from "./httpService";
 
 export const UploadedFiles = (token) => {
   return httpService.get(`${config.baseUrl}/api/upload/files`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 export const AddArticleService = (token, data) => {
   return httpService.post(`${config.baseUrl}/api/posts/create`, data, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 export const ListArticlesService = (token) => {
   return httpService.post(
     `${config.baseUrl}/api/posts`,
@@ -24,51 +24,51 @@ export const ListArticlesService = (token) => {
         Authorization: `Bearer ${token}`,
       },
     }
-  )
-}
+  );
+};
 export const DeleteArticleService = (token, id) => {
   return httpService.post(
     `${config.baseUrl}/api/posts/delete/${id}`,
     {},
     {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
     }
-  )
-}
+  );
+};
 export const SingleArticleService = (token, id) => {
   return httpService.get(`${config.baseUrl}/api/posts/single/${id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 
 export const CatListService = (token) => {
-  return httpService.get(`${config.baseUrl}/api/posts/cats/list`, {
+  return httpService.get(`${config.baseUrl}/api/posts/cats/list/all`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 
 export const CreateCatService = (token, data) => {
   return httpService.post(`${config.baseUrl}/api/posts/cats/create`, data, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 
 export const ListTagsService = (token) => {
   return httpService.get(`${config.baseUrl}/api/tags/list`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 
 export const DeleteTagService = (token, id) => {
   return httpService.get(
@@ -76,32 +76,32 @@ export const DeleteTagService = (token, id) => {
 
     {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
     }
-  )
-}
+  );
+};
 
 export const DeleteArticleCatService = (token, data) => {
   return httpService.get(`${config.baseUrl}/api/posts/cats/delete/${data}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 export const EditArticleService = (token, data, id) => {
   return httpService.post(`${config.baseUrl}/api/posts/edit/${id}`, data, {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 export const EditArticleCatService = (token, data, id) => {
   return httpService.post(`${config.baseUrl}/api/posts/cats/edit/${id}`, data, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
