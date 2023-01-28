609cea9427d92c689dd1105f7721bb787ad3cc22
Sepehr Sattari <40902851+seqehr@users.noreply.github.com>
2023-01-28 17:37:31 +0330

    delete user is added to admin panel

--- src/pages/Members/TableRow.jsx
+++ src/pages/Members/TableRow.jsx
@@ -1,29 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { AiFillEye } from 'react-icons/ai'
-import { SiSpringsecurity } from 'react-icons/si'
-import { MdAutorenew } from 'react-icons/md'
-import { Link } from 'react-router-dom'
-import useToken from '../../hooks/useToken'
-import { userJustify } from '../../services/MemberServices'
-import * as shamsi from 'shamsi-date-converter'
+import React, { useEffect, useState } from "react";
+import { AiFillEye } from "react-icons/ai";
+import { SiSpringsecurity } from "react-icons/si";
+import { MdAutorenew } from "react-icons/md";
+import { Link } from "react-router-dom";
+import useToken from "../../hooks/useToken";
+import { toast } from "react-toastify";
+import { userJustify } from "../../services/MemberServices";
+import * as shamsi from "shamsi-date-converter";
 // css
-import style from './TableRow.module.scss'
-import UserModal from './UserModal'
-import { ListCoursesService } from '../../services/CourseServices'
+import style from "./TableRow.module.scss";
+import UserModal from "./UserModal";
+import { ListCoursesService } from "../../services/CourseServices";
+import { DeleteUser } from "../../services/CourseServices";
 // Icons
-import { BsEye } from 'react-icons/bs'
-import { BiEdit } from 'react-icons/bi'
+import { BsEye } from "react-icons/bs";
+import { BiEdit } from "react-icons/bi";
 const TableRow = ({ name, phone, id, key, fullDetails }) => {
-  const { token } = useToken()
-  const [isOpeModal, setIsOpeModal] = useState(false)
+  const { token } = useToken();
+  const [isOpeModal, setIsOpeModal] = useState(false);
+  const SendDeleteUser = (id) => {
+    console.log(id);
+    DeleteUser(id, token).then((res) => {
+      toast.success("کاربر با موفقیت حذف شد");
+      // get list agai
+    });
+  };
 
   return (
     <tr key={key} className={`${style.walletTableRow} `}>
-      <td className='py-2 pr-4'>
-        <div className='flex items-center gap-2'>
+      <td className="py-2 pr-4">
+        <div className="flex items-center gap-2">
           <div>
             <div>
-              <span className='text-sm  text-black sm:text-base dark:text-white'>
+              <span className="text-sm text-black sm:text-base dark:text-white">
                 {name}
               </span>
             </div>
@@ -31,74 +40,82 @@ const TableRow = ({ name, phone, id, key, fullDetails }) => {
           </div>
         </div>
       </td>
-      <td className='py-2'>
+      <td className="py-2">
         <div>
-          <div className='flex gap-2 items-center text-sm text-center text-black sm:text-base dark:text-white'>
+          <div className="flex items-center gap-2 text-sm text-center text-black sm:text-base dark:text-white">
             <span>{fullDetails.id}</span>
           </div>
         </div>
       </td>
-      <td className='py-2'>
+      <td className="py-2">
         <div>
-          <div className='flex gap-2 text-sm text-black sm:text-base dark:text-white'>
+          <div className="flex gap-2 text-sm text-black sm:text-base dark:text-white">
             <span>{phone}</span>
           </div>
         </div>
-      </td>{' '}
-      <td className='py-2'>
+      </td>{" "}
+      <td className="py-2">
         <div>
-          <div className='flex gap-2 text-sm text-black sm:text-base dark:text-white'>
+          <div className="flex gap-2 text-sm text-black sm:text-base dark:text-white">
             <span>
               {shamsi
                 .gregorianToJalali(fullDetails.created_at)
                 .map((i) => i)
-                .join('/')}
+                .join("/")}
             </span>
           </div>
         </div>
       </td>
-      <div className='float-left'>
-        <td className='px-1 py-3 sm:py-2 sm:px-1'>
-          <div className='flex flex-col justify-end gap-2 sm:flex-row'>
+      <div className="float-left">
+        <td className="px-1 py-3 sm:py-2 sm:px-1">
+          <div className="flex flex-col justify-end gap-2 sm:flex-row">
             <Link
               to={`/permission/member/${id}`}
-              className='px-2 sm:px-5 py-1 text-sm text-white sm:text-base bg-ethereum-light dark:bg-ethereum-dark rounded-lg flex '
+              className="flex px-2 py-1 text-sm text-white rounded-lg sm:px-5 sm:text-base bg-ethereum-light dark:bg-ethereum-dark "
             >
-              {`مجوز ها `} <SiSpringsecurity className='text-xl mr-2 pt-1' />
+              {`مجوز ها `} <SiSpringsecurity className="pt-1 mr-2 text-xl" />
             </Link>
+            <div
+              onClick={() => {
+                SendDeleteUser(id);
+              }}
+              className="flex px-2 py-1 text-sm text-white rounded-lg sm:px-5 sm:text-base bg-ethereum-light dark:bg-ethereum-dark "
+            >
+              {`حذف کاربر`} <SiSpringsecurity className="pt-1 mr-2 text-xl" />
+            </div>
           </div>
         </td>
-        <td className='px-1 py-3 sm:py-2 sm:px-1'>
-          <div className='flex flex-col justify-end gap-2 sm:flex-row'>
+        <td className="px-1 py-3 sm:py-2 sm:px-1">
+          <div className="flex flex-col justify-end gap-2 sm:flex-row">
             <Link
               to={`/courses/member/${id}`}
-              className='px-2 sm:px-5 py-1 text-sm text-white sm:text-base bg-bitcoin-light dark:bg-bitcoin-dark rounded-lg '
+              className="px-2 py-1 text-sm text-white rounded-lg sm:px-5 sm:text-base bg-bitcoin-light dark:bg-bitcoin-dark "
             >
               {`دوره ها`}
             </Link>
           </div>
         </td>
 
-        <td className='px-1 py-3 sm:py-2 sm:px-1'>
+        <td className="px-1 py-3 sm:py-2 sm:px-1">
           <div
-            className='flex flex-col justify-end gap-2 sm:flex-row'
+            className="flex flex-col justify-end gap-2 sm:flex-row"
             onClick={() => setIsOpeModal(!isOpeModal)}
           >
-            <p className='cursor-pointer px-2 sm:px-5 py-1 text-sm text-white sm:text-base bg-bitcoin-light dark:bg-bitcoin-dark rounded-lg  flex justify-center items-center gap-2'>
-              نمایش <AiFillEye className='text-xl' />
+            <p className="flex items-center justify-center gap-2 px-2 py-1 text-sm text-white rounded-lg cursor-pointer sm:px-5 sm:text-base bg-bitcoin-light dark:bg-bitcoin-dark">
+              نمایش <AiFillEye className="text-xl" />
             </p>
           </div>
         </td>
-        <td className='px-1 py-3 sm:py-2 sm:px-1'>
+        <td className="px-1 py-3 sm:py-2 sm:px-1">
           <div
-            className='flex flex-col justify-end gap-2 sm:flex-row'
+            className="flex flex-col justify-end gap-2 sm:flex-row"
             onClick={() => setIsOpeModal(!isOpeModal)}
           >
             <Link
               to={`/member/edit/${fullDetails.id}`}
-              className='cursor-pointer px-2 sm:px-5 py-1 text-sm text-white sm:text-base bg-bitcoin-light dark:bg-bitcoin-dark rounded-lg  flex justify-center items-center gap-2'
+              className="flex items-center justify-center gap-2 px-2 py-1 text-sm text-white rounded-lg cursor-pointer sm:px-5 sm:text-base bg-bitcoin-light dark:bg-bitcoin-dark"
             >
-              ویرایش <BiEdit className='text-xl' />
+              ویرایش <BiEdit className="text-xl" />
             </Link>
           </div>
         </td>
@@ -108,7 +125,7 @@ const TableRow = ({ name, phone, id, key, fullDetails }) => {
         <UserModal fullDetails={fullDetails} close={setIsOpeModal} />
       )}
     </tr>
-  )
-}
+  );
+};
 
-export default TableRow
+export default TableRow;
--- src/services/CourseServices.js
+++ src/services/CourseServices.js
@@ -1,60 +1,67 @@
-import axios from 'axios'
-import config from './config.json'
-import httpService from './httpService'
+import axios from "axios";
+import config from "./config.json";
+import httpService from "./httpService";
 
 export const UploadedFiles = (token) => {
   return httpService.get(`${config.baseUrl}/api/upload/files`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 export const AddCourseService = (token, data) => {
   return httpService.post(`${config.baseUrl}/api/courses/create`, data, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 export const ListCoursesService = (token) => {
   return httpService.get(`${config.baseUrl}/api/courses/archive`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
+export const DeleteUser = (id, token) => {
+  return httpService.get(`${config.baseUrl}/api/admin/user/delete/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
 export const DeleteCoursesService = (token, id) => {
   return httpService.post(
     `${config.baseUrl}/api/courses/delete/${id}`,
     {},
     {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
     }
-  )
-}
+  );
+};
 
 export const SingleCourseService = (token, id) => {
   return httpService.get(`${config.baseUrl}/api/courses/single/${id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 export const EditCourseService = (token, data, id) => {
   return httpService.post(`${config.baseUrl}/api/courses/edit/${id}`, data, {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
 export const SerchCourseService = (token, text) => {
   return httpService.get(`${config.baseUrl}/api/courses/search/${text}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  })
-}
+  });
+};
